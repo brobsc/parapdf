258da07bdafb986c3fcae76da7c890d407aef16c
Rob <bruno.robsiq@gmail.com>
2019-07-24 18:08:00 -0300

    Add jimageio dependency and restruct tools.pdf

--- src/smart_pdf/tools/pdf.clj
+++ src/smart_pdf/tools/pdf.clj
@@ -76,6 +76,11 @@
          (inc)
          (subs s))))
 
+(defn pdf?
+  "Returns `true` when `f` has pdf extension."
+  [f]
+  (= "pdf" (ext f)))
+
 (defn strip-ext
   "Removes possible extension from `arg` name.
 
@@ -85,27 +90,9 @@
   [arg]
   (let [s (->name arg)
         idx (string/last-index-of s ".")]
-    (if-not (= -1 idx)
+    (if idx
       (subs s 0 idx)
       s)))
-
-(defn pdf->imgs
-  "Converts a `pdf` File to many images compressed to `quality`, returning a `seq` of them."
-  [^File pdf ^Float quality]
-  (with-open [doc (PDDocument/load pdf)]
-    (let [renderer (doto (PDFRenderer. doc)
-                     (.setSubsamplingAllowed true))]
-      (doall
-       (map (fn [page]
-              (let [bim (.renderImageWithDPI renderer page 72)
-                    temp-file (File/createTempFile
-                                (str (strip-ext pdf)
-                                     "-"
-                                     (inc page)
-                                     "-") ".jpg")]
-                (ImageIOUtil/writeImage bim (.getPath temp-file) 72 quality)
-                temp-file))
-            (range (.getNumberOfPages doc)))))))
 
 (defn fit
   "Returns [width height] of `a` fit inside `b`.
@@ -145,13 +132,32 @@
   [in out]
   (m/merge-pdfs :input in :output out))
 
+(defn pdf->imgs
+  "Converts a `pdf` File to many images compressed to `quality`, returning a `seq` of them."
+  [^File pdf ^Float quality]
+  (with-open [doc (PDDocument/load pdf)]
+    (let [renderer (doto (PDFRenderer. doc)
+                     (.setSubsamplingAllowed true))]
+      (doall
+       (map (fn [page]
+              (let [bim (.renderImageWithDPI renderer page 72)
+                    temp-file (File/createTempFile
+                                (str (strip-ext pdf)
+                                     "-"
+                                     (inc page)
+                                     "-") ".jpg")]
+                (ImageIOUtil/writeImage bim (.getPath temp-file) 72 quality)
+                temp-file))
+            (range (.getNumberOfPages doc)))))))
+
+
 (defn file->pdf
   "Returns a pdf `File` result of `f` converted to it, or `f` if it's already one.
 
   Currently only processes image files (png/jpeg)."
   [f]
   (let [f (io/file f)]
-    (if-not (= "pdf" (ext f))
+    (if-not (pdf? f)
       (with-open [doc (PDDocument.)]
         (let [img-name (strip-ext f)
               temp-file (File/createTempFile (str img-name "-pdf") ".pdf")
@@ -178,9 +184,9 @@
 (defn optimize-pdf
   "Returns compressed `pdf`.
 
-  Attempts to optimize by reducing its quality by 30%."
+  Attempts to optimize by reducing its quality by 10%."
   [^File pdf]
-  (let [images (pdf->imgs pdf 0.7)
+  (let [images (pdf->imgs pdf 0.9)
         temp-file (File/createTempFile
                     (str (strip-ext pdf) "-opt")
                     ".pdf")
