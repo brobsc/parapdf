c424e8d1a79e115a1aa0723aa213f7716797ac65
Rob <bruno.robsiq@gmail.com>
2019-07-25 18:04:57 -0300

    First attempt at improving pdf compression

--- src/smart_pdf/tools/pdf.clj
+++ src/smart_pdf/tools/pdf.clj
@@ -8,22 +8,35 @@
     [pdfboxing.merge :as m])
   (:import
     [java.io
-     File]
+     File
+     PipedInputStream
+     InputStream
+     ByteArrayOutputStream
+     PipedOutputStream]
     [org.apache.pdfbox.pdmodel
      PDDocument
      PDPage
      PDPageContentStream]
     [org.apache.pdfbox.tools.imageio
      ImageIOUtil]
+    [org.apache.pdfbox.multipdf
+     PDFMergerUtility]
     [org.apache.pdfbox.rendering
-     PDFRenderer]))
+     PDFRenderer]
+    [org.apache.pdfbox.pdmodel.graphics.image
+     PDImageXObject]))
 
 (set! *warn-on-reflection* true)
 
 (defn join
   "Joins a vector of pdfs `in` into `out`."
   [in out]
-  (m/merge-pdfs :input (map fpath in) :output out))
+  (let [merger (doto
+                 (PDFMergerUtility.)
+                 (.setDestinationFileName (fpath out)))]
+    (doseq [f in]
+      (.addSource merger ^InputStream f))
+    (.mergeDocuments merger)))
 
 (defn pdf->imgs
   "Converts a `pdf` File to many images compressed to `quality`, returning a `seq` of them."
@@ -32,24 +45,29 @@
     (let [renderer (doto (PDFRenderer. doc)
                      (.setSubsamplingAllowed true))]
       (doall
-       (map (fn [page]
-              (let [bim (.renderImageWithDPI renderer page 72)
-                    temp-file (temp-file-from pdf :ext "jpg" :number (inc page))]
-                (ImageIOUtil/writeImage bim (fpath temp-file) 72 quality)
-                temp-file))
-            (range (.getNumberOfPages doc)))))))
+        (map (fn [page]
+               (let [bim (.renderImageWithDPI renderer page 96)
+                     pis (PipedInputStream.)
+                     pos (PipedOutputStream. pis)]
+                 (future (ImageIOUtil/writeImage bim "jpeg"
+                                                 pos
+                                                 96 quality)
+                         (.close pos))
+                 pis))
+             (range (.getNumberOfPages doc)))))))
 
-
-(defn file->pdf
-  "Returns a pdf `File` result of `f` converted to it, or `f` if it's already one.
-
-  Currently only processes image files (png/jpeg)."
-  [f]
-  (let [f (io/file f)]
-    (if-not (pdf? f)
+(defn stream->pdf
+  [^PipedInputStream in]
+  (let [pis (PipedInputStream.)
+        pos (PipedOutputStream. pis)]
+    (future
       (with-open [doc (PDDocument.)]
-        (let [temp-file (temp-file-from f :ext "pdf")
-              pd-img (pd-img-from-file f doc)
+        (let [b-array (.readAllBytes in)
+              _ (.close in)
+              pd-img (PDImageXObject/createFromByteArray
+                       doc
+                       b-array
+                       nil)
               page (PDPage. A4)
               [width height] (fit-in-a4 pd-img)
               [x y] (center-in-a4 [width height])]
@@ -65,20 +83,47 @@
                             (float y)
                             (float width)
                             (float height))))
-          (.save doc temp-file)
-          temp-file))
-      f)))
+          (.save doc pos)
+          (.close pos))))
+    pis))
+
+(defn file->pdf
+  "Returns a pdf `File` result of `f` converted to it, or `f` if it's already one.
+
+  Currently only processes image files (png/jpeg)."
+  [f]
+  (let [f (io/file f)]
+      (if-not (pdf? f)
+        (with-open [doc (PDDocument.)]
+          (let [temp-file (temp-file-from f :ext "pdf")
+                pd-img (pd-img-from-file f doc)
+                page (PDPage. A4)
+                [width height] (fit-in-a4 pd-img)
+                [x y] (center-in-a4 [width height])]
+            (.addPage doc page)
+            (with-open [content-stream (PDPageContentStream.
+                                         doc
+                                         page
+                                         true
+                                         false)]
+              (-> content-stream
+                  (.drawImage pd-img
+                              (float x)
+                              (float y)
+                              (float width)
+                              (float height))))
+            (.save doc temp-file)
+            temp-file))
+        f)))
 
 (defn compress
   "Returns compressed `pdf` `File`.
 
   Reduces dpi to 72 and quality by 10%."
   [^File pdf]
-  (let [images (pdf->imgs pdf 0.9)
-        temp-file (temp-file-from pdf)
-        path (fpath temp-file)]
+  (let [images (pdf->imgs pdf 0.8)
+        temp-file (temp-file-from pdf)]
     (as-> images images
-      (doall (pmap file->pdf images))
-      (map fpath images)
-      (join images path))
+      (pmap stream->pdf images)
+      (join images temp-file))
      temp-file))
