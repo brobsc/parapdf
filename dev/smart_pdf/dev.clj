c4cb6fbd8ac343f5360d45098382b10527852f5f
Rob <bruno.robsiq@gmail.com>
2019-07-26 13:26:45 -0300

    Update deps and add refactor-nrepl

--- dev/smart_pdf/dev.clj
+++ dev/smart_pdf/dev.clj
@@ -1,18 +1,24 @@
 (ns smart-pdf.dev
-  (:require [smart-pdf.core]
+  (:require [cider.nrepl :refer [cider-middleware]]
             [nrepl.server :as server]
-            [reply.main :as reply]))
+            [reply.main :as reply]
+            [refactor-nrepl middleware]
+            [smart-pdf.core]))
 
-(defn nrepl-handler []
-  (require 'cider.nrepl)
-  (ns-resolve 'cider.nrepl 'cider-nrepl-handler))
+(def wrapped-handler
+  (apply server/default-handler
+         (cons #'refactor-nrepl.middleware/wrap-refactor
+               (map resolve cider-middleware))))
 
 (defn -main []
   (println "Dev started...")
   (let [port (+ 6690 (rand-int 100))]
-    (println (format "Starting nREPL server at port %d" port))
     (spit (doto (clojure.java.io/file "./.nrepl-port") .deleteOnExit) port)
-    (server/start-server :port port :handler (nrepl-handler))
-    (reply/launch-nrepl {:attach (str port)}))
+    (with-open [s (server/start-server :port port :handler wrapped-handler)]
+      (println (format "Started nREPL server at port %d" port))
+      (reply/launch-nrepl {:attach (str port)})))
   (shutdown-agents)
   (System/exit 0))
+
+;; /usr/local/bin/clojure -Sdeps '{:deps {nrepl {:mvn/version "0.6.0"} refactor-nrepl {:mvn/version "2.4.0"} cider/cider-nrepl {:mvn/version "0.21.2-SNAPSHOT"}}}' -R:dev -m nrepl.cmdline --middleware '["refactor-nrepl.middleware/wrap-refactor", "cider.nrepl/cider-middleware"]'
+
